867642b705f501b5973a3f80c8265b5552c9f0c9
yesongsheng <yesongsheng@163.com>
2021-06-01 20:25:54 +0800

    [Fixed] try to resolve complix

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
-/* eslint-disable */
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-=======
 declare module '*.vue' {
   import { DefineComponent } from 'vue'
->>>>>>> a3cb8fa ([Function] Complete Project)
   const component: DefineComponent<{}, {}, any>
   export default component
 }
